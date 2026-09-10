b10e130ae559cee08e3d043d467892c0e2e4f6bf
wchichura-maker <wchichura@gmail.com>
2026-09-10 19:38:45 -0300

    Register encounter initiative tests

--- src/game/tests/runTests.ts
+++ src/game/tests/runTests.ts
@@ -1,78 +1,23 @@
-import {
-  runRulesTests
-} from "./RulesTest";
-
-import {
-  runRangeRulesTests
-} from "./RangeRulesTest";
-
-import {
-  runPathfindingTests
-} from "./PathfindingTest";
-
-import {
-  runFiveFootStepTests
-} from "./FiveFootStepTest";
-
-import {
-  runStabilizationTests
-} from "./StabilizationTest";
-
-import {
-  runSavingThrowTests
-} from "./SavingThrowTest";
-
-import {
-  runConditionStateTests
-} from "./ConditionStateTest";
-
-import {
-  runDisabledStateTests
-} from "./DisabledStateTest";
-
-import {
-  runHitPointTransitionTests
-} from "./HitPointTransitionTest";
-
-import {
-  runCoupDeGraceTests
-} from "./CoupDeGraceTest";
-
-import {
-  runCoupDeGraceIntegrationTests
-} from "./CoupDeGraceIntegrationTest";
-
-import {
-  runCombatResolutionTests
-} from "./CombatResolutionTest";
-
-import {
-  runSocialRulesTests
-} from "./SocialRulesTest";
-
-import {
-  runSocialCombatIntegrationTests
-} from "./SocialCombatIntegrationTest";
-
-import {
-  runAITests
-} from "./AITest";
-
-import {
-  runCombatMovementTests
-} from "./CombatMovementTest";
-
-import {
-  runWithdrawRulesTests
-} from "./WithdrawRulesTest";
-
-import {
-  runCombatMovementIntegrationTests
-} from "./CombatMovementIntegrationTest";
-
-import {
-  runGameCoreServerIntegrationTests
-} from "./GameCoreServerIntegrationTest";
+import { runRulesTests } from "./RulesTest";
+import { runRangeRulesTests } from "./RangeRulesTest";
+import { runPathfindingTests } from "./PathfindingTest";
+import { runFiveFootStepTests } from "./FiveFootStepTest";
+import { runStabilizationTests } from "./StabilizationTest";
+import { runSavingThrowTests } from "./SavingThrowTest";
+import { runConditionStateTests } from "./ConditionStateTest";
+import { runDisabledStateTests } from "./DisabledStateTest";
+import { runHitPointTransitionTests } from "./HitPointTransitionTest";
+import { runCoupDeGraceTests } from "./CoupDeGraceTest";
+import { runCoupDeGraceIntegrationTests } from "./CoupDeGraceIntegrationTest";
+import { runCombatResolutionTests } from "./CombatResolutionTest";
+import { runSocialRulesTests } from "./SocialRulesTest";
+import { runSocialCombatIntegrationTests } from "./SocialCombatIntegrationTest";
+import { runAITests } from "./AITest";
+import { runCombatMovementTests } from "./CombatMovementTest";
+import { runWithdrawRulesTests } from "./WithdrawRulesTest";
+import { runCombatMovementIntegrationTests } from "./CombatMovementIntegrationTest";
+import { runEncounterInitiativeTests } from "./EncounterInitiativeTest";
+import { runGameCoreServerIntegrationTests } from "./GameCoreServerIntegrationTest";
 
 runRulesTests();
 runRangeRulesTests();
@@ -92,4 +37,5 @@ runAITests();
 runCombatMovementTests();
 runWithdrawRulesTests();
 runCombatMovementIntegrationTests();
+runEncounterInitiativeTests();
 await runGameCoreServerIntegrationTests();
